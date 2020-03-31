f6830e9b33ebb618cb55c3c14bfcb9c8aac3e129
Mageshwaran <35984541+MageshwaranM@users.noreply.github.com>
2020-03-31 22:36:44 +0530

    Update client1.py

--- client1.py
+++ client1.py
@@ -3,7 +3,7 @@ s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 complete_info = ''
 s.connect((socket.gethostname(), 1025))
 while True:
-    msg = s.recv(10)
+    msg = s.recv(8)
     if len(msg)<=0:
         break
     complete_info += msg.decode("utf-8")
